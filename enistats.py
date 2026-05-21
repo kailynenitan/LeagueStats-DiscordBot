28369f87d5b3d067ff11548c5053b5138411a479
unknown <kailyn.enitan@gmail.com>
2026-05-21 00:39:33 -0700

    Attempted to add bot commands. Need to look into client vs. bot in same file for discord.py api

--- enistats.py
+++ enistats.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
@@ -13,13 +14,25 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
+'''@client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if len(message.attachments) > 0:
+        await message.channel.send('Attachment detected')
+
+'''
+
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def test(ctx):
+    await ctx.send('bot command works')
 
 client.run(os.getenv("BOT_TOKEN"))
 

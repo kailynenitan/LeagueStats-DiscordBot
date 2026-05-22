22a30bbf23247ca27962224828e86278e78a21bc
unknown <kailyn.enitan@gmail.com>
2026-05-21 21:44:32 -0700

    Added command to save an attachment to the current working directory

--- enistats.py
+++ enistats.py
@@ -8,27 +8,6 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-'''
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    if len(message.attachments) > 0:
-        await message.channel.send('Attachment detected')
-
-'''
-
-
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
@@ -39,5 +18,11 @@ async def on_ready():
 async def test(ctx, arg):
     await ctx.send(arg)
 
-bot.run(os.getenv("BOT_TOKEN"))
+@bot.command()
+async def upload(ctx, attachment: discord.Attachment):
+    #await attachment.save('C:/Users/Kailyn/Desktop/LeagueStats-DiscordBot{stats.jpg}')
+    path = os.path.join(os.getcwd(), 'stats.jpg')
+    await attachment.save(path)
+    await ctx.send(f'You uploaded <{attachment.url}>')
 
+bot.run(os.getenv("BOT_TOKEN"))

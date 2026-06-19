3dec6494ffa2b1430bbcb0071bd78fa0684c1038
unknown <kailyn.enitan@gmail.com>
2026-06-18 23:34:38 -0700

    Edited regions to be more uniform. Created read_region crop an image to a region of stats and read in the information line by line

--- enistats.py
+++ enistats.py
@@ -4,6 +4,7 @@ import discord
 import numpy as np
 import os
 import pytesseract
+import string
 from concurrent.futures import ProcessPoolExecutor
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -12,19 +13,19 @@ from PIL import Image
 IMG_NAME = 'stats.jpg'
 IMG_PATH = os.path.join(os.getcwd(), IMG_NAME)
 REGIONS = {
-    't1_unames': (326, 619, 360, 380),
-    't1_levels': (176, 620, 65, 390),
-    't1_kda': (1186, 625, 275, 390),
-    't1_cs': (1494, 619, 120, 390),
-    't1_gold': (1635, 619, 200, 400),
+    't1_unames': (326, 620, 360, 400),
+    't1_levels': (176, 620, 65, 400),
+    't1_kda': (1186, 620, 275, 400),
+    't1_cs': (1494, 620, 120, 400),
+    't1_gold': (1635, 620, 200, 400),
     
-    't2_unames': (327, 1110, 360, 380),
-    't2_levels': (178, 1110, 65, 390),
-    't2_kda': (1188, 1115, 275, 390),
-    't2_cs': (1491, 1109, 120, 390),
-    't2_gold': (1634, 1111, 200, 400),
+    't2_unames': (327, 1110, 360, 400),
+    't2_levels': (178, 1110, 65, 400),
+    't2_kda': (1188, 1110, 275, 400),
+    't2_cs': (1491, 1110, 120, 400),
+    't2_gold': (1634, 1110, 200, 400),
     
-    'game_result': (203, 220, 248, 74)
+    'game_result': (203, 220, 250, 75)
 }
 
 load_dotenv()
@@ -33,9 +34,37 @@ pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tessera
 
 
 # WIP
-def read_image():
-    # Return a string of text from IMG_PATH
+def read_region(img, region):
+    # Read the text in a specified region of an image
 
+    r_x = region[0]
+    r_y = region[1]
+    r_w = region[2]
+    r_h = region[3]
+    line_height = 80
+    whitelist = string.digits + string.ascii_letters + '/'
+    config = f'--psm 7 -c tessedit_char_whitelist={whitelist}'
+    
+    crop = img[r_y : r_y + r_h, r_x : r_x + r_w]
+
+    if region == REGIONS['game_result']:
+        return pytesseract.image_to_string(crop, config = config)
+    
+    text = ''
+    for line in range(0, line_height * 5, line_height):
+        cv2.imshow('one line', crop[line: line + line_height, 0: r_w])
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        line_text = pytesseract.image_to_string(
+            crop[line: line + line_height, 0: r_w],
+            config = config)
+        text = text + '\n' + line_text
+
+    return text
+    
+
+# WIP
+def read_stats():
     if not os.path.exists(IMG_PATH):
         return None
 
@@ -43,14 +72,15 @@ def read_image():
     img = cv2.imread(IMG_NAME)
     gray = cv2.bitwise_not(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
     resized = cv2.resize(gray, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
-    
+
+    '''
     cv2.imshow('current image', resized)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    '''
     
-    text = pytesseract.image_to_string(resized)
-    return text
-
+    # text = pytesseract.image_to_string(resized)
+    return read_region(resized, REGIONS['t1_unames'])
 
 
 
@@ -90,11 +120,13 @@ class EditCog(commands.Cog):
 
         await attachment.save(IMG_PATH)
 
-        await ctx.send('Testing pytesseract image reading...\n\n')
+        await ctx.send('Testing pytesseract image reading...\n')
         #await ctx.send(pytesseract.image_to_string(Image.open(IMG_NAME)))
         loop = asyncio.get_running_loop()
-        img_text = await loop.run_in_executor(self.bot.executor, read_image)
+        img_text = await loop.run_in_executor(self.bot.executor, read_stats)
         await ctx.send(img_text)
+
+        await os.remove('stats.jpg')
         await ctx.send('Complete!')
 
 

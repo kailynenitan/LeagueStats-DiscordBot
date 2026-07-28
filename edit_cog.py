7e947e0b1835ee0b8954cb3a628a022d63e7ba6f
unknown <kailyn.enitan@gmail.com>
2026-07-27 23:55:15 -0700

    feat(insert_db): add insert_db command that adds one player to the database
    This is the first operating insert command in this bot and the first insert command that I have written :p

--- edit_cog.py
+++ edit_cog.py
@@ -5,6 +5,7 @@ import numpy as np
 import sqlite3
 from concurrent.futures import ProcessPoolExecutor
 from discord.ext import commands
+from pathlib import Path
 
 import config
 import reader
@@ -35,10 +36,10 @@ class EditCog(commands.Cog):
             if not attachment.content_type.startswith('image'):
                 await ctx.send('ERR: Wrong attachment type.')
                 return
-
+        
         # Save and edit an image to make it easier for the OCR to read the text
         await attachment_list[0].save(config.IMG_PATH)
-        text_reader = reader.ImageReader()
+        text_reader = reader.ImageReadfer()
         processed = text_reader.process_image()
         
         img_text = text_reader.read_region(processed, arg)
@@ -53,7 +54,23 @@ class EditCog(commands.Cog):
 
 
     @commands.command()
-    async def insert_db(self, ctx):
-        conn = sqlite3.connect('test_database.db')
+    async def insert_db(self, ctx, arg):
+        db_folder = Path(config.DB_FOLDER)
+        db_file = db_folder / config.DB_FILE
+        await ctx.send(f'path: {db_file}')
+        
+        conn = sqlite3.connect(db_file)
         cur = conn.cursor()
-        cur.execute('CREATE TABLE IF NOT EXISTS test_table.')
+        try:
+            # Add a new player to the database
+            conn.execute(
+                'INSERT INTO player_table (league_username) VALUES (?);',
+                (arg,)
+            )
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f'Database insert failed: {e}')
+        finally:   
+            conn.close()
+
+        return

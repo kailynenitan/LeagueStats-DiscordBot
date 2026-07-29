8286877b635653c54cbdae1ff3a65a337eae2aaf
unknown <kailyn.enitan@gmail.com>
2026-07-28 20:04:42 -0700

    feat($get_player_names): add new command to print all names associated with a player
    This is the first get command for this project. $get_player_names is a command that will be changed later to return the row in a prettier format.

--- get_cog.py
+++ get_cog.py
@@ -1,5 +1,9 @@
 import discord
+import sqlite3
 from discord.ext import commands
+from pathlib import Path
+
+import config
 
 # WIP
 class GetCog(commands.Cog):
@@ -9,3 +13,30 @@ class GetCog(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def get_player_names(self, ctx, arg):
+        # Print all names associated with a player
+        
+        db_folder = Path(config.DB_FOLDER)
+        db_file = db_folder / config.DB_FILE
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+
+        try:
+            cur.execute(
+                'SELECT * FROM player_table WHERE league_username = ?;',
+                (arg,)
+            )
+
+            output = cur.fetchall()
+            for row in output:
+                await ctx.send(row)
+            
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f'Database SELECT failed: {e}')
+        finally:
+            conn.close()
+
+        return

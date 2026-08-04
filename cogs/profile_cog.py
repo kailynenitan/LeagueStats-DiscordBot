a219ec074e8072e53b4052a9fb67f898a760ef29
Kailyn Enitan <kailyn.enitan@gmail.com>
2026-08-04 12:56:01 -0700

    refactor(profile): change database method calls
    databasehandler.py was changed to make execute_query() more robust. Some
    calls to execute_query needed to change to accomodate for these changes.

--- cogs/profile_cog.py
+++ cogs/profile_cog.py
@@ -16,66 +16,54 @@ class ProfileCog(commands.Cog):
 
     @commands.command(name='add_player')
     async def insert_player(self, league_username, discord_username=None, nickname=None):
-        db_folder = Path(config.DB_FOLDER)
-        db_file = db_folder / config.DB_FILE
         
         sql_statement = (
-                'INSERT INTO player_table (league_username)'
-                'VALUES (?);',
-                (league_username,)
+            'INSERT INTO player_table (league_username)'
+            'VALUES (?);'
         )
+        params = (league_username,)
 
         if (discord_username is not None):
             sql_statement = (
                 'INSERT INTO player_table (league_username, discord_username);'
-                'VALUES (?, ?);',
-                (league_username, discord_username)
+                'VALUES (?, ?);'
             )
+            params = (league_username, discord_username)
 
-        if (name is not None):
+        if (nickname is not None):
+            sql_statement = (
+                'INSERT INTO player_table (league_username, nickname);'
+                'VALUES (?, ?);'
+            )
+            params = (league_username, nickname)
+
+
+        if (discord_username is not None and nickname is not None):
             sql_statement = (
                 'INSERT INTO player_table (league_username, discord_username, nickname);'
-                'VALUES (?, ?, ?);',
-                (league_username, discord_username, nickname)
+                'VALUES (?, ?, ?);'
             )
+            params = (league_username, discord_username, nickname)
 
-        try:
-            conn = sqlite3.connect(db_file)
-            cur = conn.cursor()
-            conn.execute(sql_statement)
-            conn.commit()
-        except sqlite3.Error as e:
-            print(f'Database insert failed: {e}')
-        finally:   
-            conn.close()
+        self.db_handler.execute_query(sql_statement, params)
 
         return
 
-    @commands.command()
-    async def get_player_names(self, ctx, arg):
+
+    @commands.command(name='print_names')
+    async def select_all_names(self, ctx, arg):
         # Print all names associated with a player
         
-        db_folder = Path(config.DB_FOLDER)
-        db_file = db_folder / config.DB_FILE
-        
         sql_statement = (
-            'SELECT * FROM player_table WHERE league_username = ?;',
-            (arg,)
+            'SELECT * FROM player_table WHERE league_username = ?;'
         )
+        params = (arg,)
 
-        try:
-            conn = sqlite3.connect(db_file)
-            cur = conn.cursor()
-            cur.execute(sql_statement)
-            
-            output = cur.fetchall()
-            for row in output:
-                await ctx.send(row)
-            
-            conn.commit()
-        except sqlite3.Error as e:
-            print(f'Database SELECT failed: {e}')
-        finally:
-            conn.close()
-
+        row = self.db_handler.execute_query(sql_statement, params, 1)
+        await ctx.send(f'All names associated with {arg}:\n')
+        await ctx.send(
+            f'League username: {row[0]}\n'
+            f'Discord username: {row[1]}\n'
+            f'Nickname: {row[2]}'
+        )
         return

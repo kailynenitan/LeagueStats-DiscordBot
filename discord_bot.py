13aa4c63d545ae13181b45766d00d1a55672b384
unknown <kailyn.enitan@gmail.com>
2026-08-04 05:31:36 -0700

    feature(databasehandler): add a new class to handle database operations
    Database handler will handle the execution of sql queries and initializing the database.

--- discord_bot.py
+++ discord_bot.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 import config
+from cogs.databasehandler import Databasehandler
 from cogs.leaderboard_cog import LeaderboardCog
 from cogs.profile_cog import ProfileCog
 from cogs.stats_cog import StatsCog
@@ -28,6 +29,7 @@ class StatsBot(commands.Bot):
         intents.message_content = True
         super().__init__(command_prefix='$', intents=intents)
         self.executor = ProcessPoolExecutor()
+        self.db_handler = None
 
     async def close(self):
         self.executor.shutdown()
@@ -38,59 +40,10 @@ class StatsBot(commands.Bot):
         await self.add_cog(ProfileCog(self))
         await self.add_cog(StatsCog(self))
 
+        self.db_handler = Databasehandler()
+        self.db_handler.create_tables()        
+
         print(f'Logged in as {self.user}')
-
-    async def on_ready(self):
-        # Create directory and database files
-        db_folder = Path(config.DB_FOLDER)
-        db_file = db_folder / config.DB_FILE
-        db_folder.mkdir(parents=True, exist_ok=True)
-
-        # Create tables in database
-        conn = sqlite3.connect(db_file)
-        cursor = conn.cursor()
-        conn.execute('PRAGMA foreign_keys=ON;')
-
-        try:
-            conn.execute("""
-                CREATE TABLE IF NOT EXISTS game_table (
-                    gameID INTEGER PRIMARY KEY,
-                    timestamp TEXT NOT NULL,
-                    mvp INTEGER,
-                    ace INTEGER,
-                    FOREIGN KEY(mvp) REFERENCES player_table(playerID),
-                    FOREIGN KEY(ace) REFERENCES player_table(playerID)
-            );""")
-            
-            conn.execute("""
-                CREATE TABLE IF NOT EXISTS player_table (
-                    playerID INTEGER PRIMARY KEY,
-                    league_username TEXT NOT NULL UNIQUE,
-                    discord_username TEXT,
-                    nickname TEXT
-            );""")
-            
-            conn.execute("""
-                CREATE TABLE IF NOT EXISTS game_player_table (
-                    gameID INTEGER,
-                    playerID INTEGER,
-                    kills INTEGER NOT NULL,
-                    deaths INTEGER NOT NULL,
-                    assists INTEGER NOT NULL,
-                    cs INTEGER NOT NULL,
-                    gold INTEGER NOT NULL,
-                    result INTEGER NOT NULL,
-                    PRIMARY KEY(gameID, playerID),
-                    FOREIGN KEY(gameID) REFERENCES game_table(gameID),
-                    FOREIGN KEY(playerID) REFERENCES player_table(playerID)
-            );""")
-            conn.commit()
-            print(f'Database initialized at: {db_file.resolve()}')
-        except sqlite3.Error as e:
-            print(f'Database init failed: {e}')
-        finally:
-            conn.close()
-
 
 
 async def main():

389d5694cf55f3ff255f79fca7c919166356ef22
unknown <kailyn.enitan@gmail.com>
2026-08-05 05:31:53 -0700

    refactor(leaderboard): add empty methods with comments to describe plan for the methods

--- cogs/leaderboard_cog.py
+++ cogs/leaderboard_cog.py
@@ -6,3 +6,18 @@ class LeaderboardCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def select_player_stat(self, ctx):
+        pass
+        # get avg of a specified stat per game for a specified player
+
+    async def select_stat_leaderboard(self, ctx):
+        pass
+        # get leaderboard of all players for their avgs of a specified stat
+
+    async def select_player_winstreak(self, ctx):
+        pass
+        # get winstreak of a specified player
+
+    async def select_winstreak_leaderboard(self, ctx):
+        pass
+        # get leaderboard of all current winstreaks in desc order

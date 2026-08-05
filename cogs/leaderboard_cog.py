fea96e967560f6faafb727f22dc89d936863a468
unknown <kailyn.enitan@gmail.com>
2026-08-04 23:03:14 -0700

    refactor(game_cog): delete insert_game

--- cogs/leaderboard_cog.py
+++ cogs/leaderboard_cog.py
@@ -6,10 +6,3 @@ class LeaderboardCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def insert_game(self, ctx):
-        sql_statement = (
-            'INSERT INTO game_table (timestamp)'
-            'VALUES (?);'
-        )
-        params = (sqlite3.datetime('now', 'localtime'),)
-        self.db_handler.execute_query(sql_statement, params)

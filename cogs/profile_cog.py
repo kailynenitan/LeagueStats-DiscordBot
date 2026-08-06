47efb88fb540d2071300fdd2179729214679187e
Kailyn Enitan <kailyn.enitan@gmail.com>
2026-08-05 17:36:35 -0700

    fix(sql): add spaces to ends of lines in sql statements

--- cogs/profile_cog.py
+++ cogs/profile_cog.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 
 import config
 
-# WIP
 class ProfileCog(commands.Cog):
     """ 
     Holds commands to handle individual player data
@@ -13,40 +12,37 @@ class ProfileCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='add_player')
-    async def insert_player(self, league_username, discord_username=None, nickname=None):
-        
-        sql_statement = (
-            'INSERT INTO player_table (league_username)'
-            'VALUES (?);'
-        )
-        params = (league_username,)
-
-        if (discord_username is not None):
+    async def insert_player(self, league_username, discord_username=None, nickname=None) -> int:
+        if ((discord_username is not None) and (nickname is not None)):
             sql_statement = (
-                'INSERT INTO player_table (league_username, discord_username);'
-                'VALUES (?, ?);'
-            )
-            params = (league_username, discord_username)
-
-        if (nickname is not None):
-            sql_statement = (
-                'INSERT INTO player_table (league_username, nickname);'
-                'VALUES (?, ?);'
-            )
-            params = (league_username, nickname)
-
-
-        if (discord_username is not None and nickname is not None):
-            sql_statement = (
-                'INSERT INTO player_table (league_username, discord_username, nickname);'
+                'INSERT INTO player_table (league_username, discord_username, nickname) '
                 'VALUES (?, ?, ?);'
             )
             params = (league_username, discord_username, nickname)
 
-        self.db_handler.execute_query(sql_statement, params)
+        elif (discord_username is not None):
+            sql_statement = (
+                'INSERT INTO player_table (league_username, discord_username) '
+                'VALUES (?, ?);'
+            )
+            params = (league_username, discord_username)
 
-        return
+        elif (nickname is not None):
+            sql_statement = (
+                'INSERT INTO player_table (league_username, nickname) '
+                'VALUES (?, ?);'
+            )
+            params = (league_username, nickname)
+
+        else:
+            sql_statement = (
+                'INSERT INTO player_table (league_username) '
+                'VALUES (?);'
+            )
+            params = (league_username,)
+
+        self.db_handler.execute_query(sql_statement, params)
+        return self.db_handler.get_lastrowid()
 
 
     @commands.command(name='print_names')
@@ -59,6 +55,10 @@ class ProfileCog(commands.Cog):
         params = (arg,)
 
         row = self.db_handler.execute_query(sql_statement, params, 1)
+        if (row is None):
+            await ctx.send('ERR: No player is associated with this name')
+            return
+
         await ctx.send(f'All names associated with {arg}:\n')
         await ctx.send(
             f'League username: {row[0]}\n'

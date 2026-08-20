06b2a3393f1f3da41a3addc6295167330f351acc
unknown <kailyn.enitan@gmail.com>
2026-08-19 21:38:24 -0700

    rename: rename views to better represent content

--- cogs/matchdata_view.py
+++ cogs/matchdata_view.py
@@ -13,21 +13,24 @@ class ValidateStatsView(discord.ui.View):
 
     def __init__(self, timeout: float = 300.0):
         super().__init__(timeout=timeout)
-        self.count=0
-
-    def _disable_all(self) -> None:
-        for item in self.children:
-            if isinstance(item, discord.ui.Button):
-                item.disabled = True
-
-    @discord.ui.button(label='Test', style=discord.ButtonStyle.green)
-    async def counter(self, inter: discord.Interaction, button: discord.ui.Button[ValidateStatsView]) -> None:
-        button.label = 'Disabled!'
-        self._disable_all()
-        await inter.response.edit_message(view=self)
-        
+        self._add_buttons()
+     
     async def on_error(
             self, interaction: discord.Interaction[discord.Client], error: Exceptionj, item: discord.ui.Item[typing.Any]) -> None:
         tb = ''.join(traceback.format_exception(type(error), error, error.__traceback__))
         message = f'An error occurred while processing the interaction for {str(item)}:\n```py\n{tb}\n```'
         await interaction.response.send_message(message)
+
+    def _add_buttons(self):
+
+     
+    def _disable_all(self) -> None:
+        for item in self.children:
+            if isinstance(item, discord.ui.Button):
+                item.disabled = True
+
+    @discord.ui.button(label=f'{self.username}', style=discord.ButtonStyle.primary)
+    async def username_button(self, inter: discord.Interaction, button: discord.ui.Button[ValidateStatsView]) -> None:
+        button.label = 'Disabled!'
+        self._disable_all()
+        # await inter.response.edit_message(view=self)

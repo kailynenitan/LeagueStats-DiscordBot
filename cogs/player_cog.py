0f0fa3a9a877c5352f5a3e607662e3e29593f8be
unknown <kailyn.enitan@gmail.com>
2026-08-25 21:02:39 -0700

    refactor: change DAO to be a class variable

--- cogs/player_cog.py
+++ cogs/player_cog.py
@@ -5,19 +5,18 @@ from cogs.player_data_view import PlayerDataView
 class PlayerCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.dao = self.bot.get_cog('PlayerDAO')
+        if not (player_dao):
+            await ctx.send('PlayerDAO cog is not loaded.')
+            return
 
-    @commands.command(name='all_names')
+    @commands.command(name='names')
     async def select_all_names(self, ctx, league_username: str):
         if (league_username is None) or (not league_username.strip()):
             await ctx.send('Please enter a valid league_username.')
             return
 
-        player_dao = self.bot.get_cog('PlayerDAO')
-        if not (player_dao):
-            await ctx.send('PlayerDAO cog is not loaded.')
-            return
-
-        row = await player_dao.select_player(league_username=league_username)
+        row = await self.dao.select_player(league_username=league_username)
         if (not row):
             await ctx.send(f'No player found with league username: \'{league_username}\'')
             return
@@ -28,5 +27,8 @@ class PlayerCommands(commands.Cog):
 
         return
 
-    async def update_player(self):
-        pass
+    @commands.command(name='change_names')
+    async def update_player_names(self, ctx, league_username: str):
+        if (league_username is None) or (not league_username.strip()):
+            await ctx.send('Please enter a valid league_username.')
+            return
